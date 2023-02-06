a70c8a9a4df37ecbcaa83d4dd8cf94e63853de6a
Ed Wang <ed555009@gmail.com>
2023-02-06 22:52:44 +0800

    fix ActivityModel

--- src/Alchemy.Notify.Api.Client/Models/Notify/ActivityModel.cs
+++ src/Alchemy.Notify.Api.Client/Models/Notify/ActivityModel.cs
@@ -6,7 +6,7 @@ public class ActivityModel
 	public string? ToAddress { get; set; }
 	public string? BlockNum { get; set; }
 	public string? Hash { get; set; }
-	public int? Value { get; set; }
+	public decimal? Value { get; set; }
 	public string? Asset { get; set; }
 	public string? Category { get; set; }
 	public RawContractModel? RawContract { get; set; }
