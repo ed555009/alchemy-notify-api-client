f99527bd8a8f248fbaa4b6703345f37a54717a4f
Ed Wang <ed555009@gmail.com>
2023-02-06 13:14:54 +0800

    fix NotifyModel

--- src/Alchemy.Notify.Api.Client/Models/Notify/NotifyModel.cs
+++ src/Alchemy.Notify.Api.Client/Models/Notify/NotifyModel.cs
@@ -1,3 +1,5 @@
+using Alchemy.Notify.Api.Client.Enums;
+
 namespace Alchemy.Notify.Api.Client.Models.Notify;
 
 public class NotifyModel
@@ -5,6 +7,6 @@ public class NotifyModel
 	public string? WebhookId { get; set; }
 	public string? Id { get; set; }
 	public DateTimeOffset? CreatedAt { get; set; }
-	public string? Type { get; set; }
+	public WebhookType? Type { get; set; }
 	public EventModel? Event { get; set; }
 }
